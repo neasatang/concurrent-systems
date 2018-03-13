277c1e6370cf7f17f68aaa3a84dba817d07a6d7c
Neasa Tang <tangn@tcd.ie>
2018-03-13 17:32:00 +0000

    Update mergesort_serial.c

--- mergesort_serial.c
+++ mergesort_serial.c
@@ -1,30 +1,87 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void merge(int list[], int l, int m, int r)
+{
+  int i, j, k;
+  int n1 = m - l + 1;
+  int n2 =  r - m;
+
+  /* create temp arrays */
+  int L[n1], R[n2];
+
+  /* Copy data to temp arrays L[] and R[] */
+  for (i = 0; i < n1; i++)
+      L[i] = arr[l + i];
+  for (j = 0; j < n2; j++)
+      R[j] = arr[m + 1+ j];
+
+  /* Merge the temp arrays back into arr[l..r]*/
+  i = 0; // Initial index of first subarray
+  j = 0; // Initial index of second subarray
+  k = l; // Initial index of merged subarray
+  while (i < n1 && j < n2)
+  {
+      if (L[i] <= R[j])
+      {
+          arr[k] = L[i];
+          i++;
+      }
+      else
+      {
+          arr[k] = R[j];
+          j++;
+      }
+      k++;
+  }
+
+  /* Copy the remaining elements of L[], if there
+     are any */
+  while (i < n1)
+  {
+      arr[k] = L[i];
+      i++;
+      k++;
+  }
+
+  /* Copy the remaining elements of R[], if there
+     are any */
+  while (j < n2)
+  {
+      arr[k] = R[j];
+      j++;
+      k++;
+  }
+}
 
 void mergesort(int list[], int l, int r){
-	int m = (l+r)/2;
-	mergesort(list, l , m);
-	mergesort(list, m+1, r);
-	mergesort(list, l,m,r);
+  if(r > l)
+  {
+    int m = (l+r)/2;
+    mergesort(list, l , m);
+    mergesort(list, m+1, r);
+    merge(list, l,m,r);
+  }
 }
 
 int main(){
-	char string[6];
-    int list[100];
-    int i = 0;
-    FILE *file;
-    file = fopen("file.txt","r");   //reads input from file
-    while(!feof(file) && i<100)
-    {
-        fgets(string,sizeof(string),file); //gets input from file
-        list[i] = atoi(string);            //convert to integer
-        i++;
-    }
-    fclose(file);
-	int l = 0;
-	int r = 99;
-	mergesort(list,l,r);
-	for(i = 0; i < 100; i++){
-		printf("%d\n",list[i]);
-	}
-	return 0;
+  char string[6];
+  int list[100];
+  int i = 0;
+  FILE *file;
+  file = fopen("file.txt","r");   //reads input from file
+  while(!feof(file) && i<100)
+  {
+      fgets(string,sizeof(string),file); //gets input from file
+      list[i] = atoi(string);            //convert to integer
+      i++;
+  }
+  fclose(file);
+  int l = 0;
+  int r = 99;
+  mergesort(list,l,r);
+  for(i = 0; i < 100; i++){
+    printf("%d\n",list[i]);
+  }
+  return 0;
 }
