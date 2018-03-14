b718bc5f3d19b6510e19963bb5694670b3a741c6
neasatang <tangn@tcd.ie>
2018-03-14 20:01:55 +0000

    more data file

---
+++ random1000000.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+	srand(time(NULL));
+	int r,i = 0;
+	FILE *f = fopen("file.txt", "w");
+	if (f == NULL)
+	{
+		printf("Error opening file!\n");
+		exit(1);
+	}
+	for(i = 0; i <= 1000000; i++)
+	{
+		r=rand()%1000000;
+		fprintf(f, "%d\n", r);
+
+	}
+	return 0;
+}
