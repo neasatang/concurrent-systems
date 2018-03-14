74d0168e040f40f0002865e7b4471c12c5ef18f8
neasatang <tangn@tcd.ie>
2018-03-14 20:01:04 +0000

    times and works perfectly

--- linearsearch_serial.c
+++ linearsearch_serial.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
+struct timespec start, end;
 
 int linear_search(int list[], int search_var){
-	for(int i = 0; i<100; i++){
+	for(int i = 0; i<1000000; i++){
 		if(list[i]==search_var)
 		{
 			//printf("%d\n",list[i]);
@@ -15,19 +18,21 @@ int linear_search(int list[], int search_var){
 
 int main(){
 	int answer = 0;
-	int search_var = 1232312;
-	char string[6];
-    int list[100];
-    int i = 0;
-    FILE *file;
-    file = fopen("file.txt","r");   //reads input from file
-    while(!feof(file) && i<100)
-    {
-        fgets(string,sizeof(string),file); //gets input from file
+	int search_var = 123231;
+	char string[15];
+  int list[1000000];
+  int i = 0;
+  FILE *file;
+  file = fopen("file.txt","r");   //reads input from file
+  while(!feof(file) && i<1000000)
+  {
+    fgets(string,sizeof(string),file); //gets input from file
 		list[i] = atoi(string);			   //convert to integer
 		i++;
-    }
+  }
 	fclose(file);
+
+	clock_gettime(CLOCK_MONOTONIC_RAW, &start);
 	answer = linear_search(list, search_var);
 	if(answer == -1)
 	{
@@ -37,5 +42,10 @@ int main(){
 	{
 		printf("%d is at index %d\n",search_var, answer);
 	}
+
+	clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+	double x = ((end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000);
+printf("Time is: %f\n", x/1000000 );
+
 	return 0;
 }
