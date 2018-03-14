98d46bd720bd235121f227f571570151689c1871
neasatang <tangn@tcd.ie>
2018-03-14 16:36:40 +0000

    random gen for 100 data

---
+++ random100.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+	srand(time(NULL));
+	int r,i = 0;
+	FILE *f = fopen("file.txt", "w");
+	if (f == NULL)
+	{
+		printf("Error opening file!\n");
+		exit(1);
+	}
+	for(i = 0; i <= 100; i++)
+	{
+		r=rand()%100;
+		fprintf(f, "%d\n", r);
+
+	}
+	return 0;
+}
